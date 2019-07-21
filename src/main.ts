772ee407dd0a0241607fdaf63251ba07ff19a3c2
Vien Dinh <maxvien@gmail.com>
2019-07-21 16:43:44 +0700

    Add apollo-server

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,40 @@
-import sum from './libs/sum';
+import { ApolloServer, gql } from 'apollo-server';
 
-console.log(sum(1, 2));
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton'
+  }
+];
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server
+  .listen()
+  .then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`); // eslint-disable-line
+  })
+  .catch(error => {
+    console.log(error); // eslint-disable-line
+  });
